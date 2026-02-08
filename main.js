67e3a1346bcb244129e985b51951bd523e449f7c
Luis Fernando <luisfer.montesblanco@outlook.com>
2026-02-08 12:00:43 -0600

    limit increseed

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ import shopifyRoutes from "./routes/shopify.js";
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(cors({
     origin: true,
